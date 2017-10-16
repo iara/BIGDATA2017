6adc4311ab9316695b9f7fa2d2ede34e55a0adbc
Iara Miranda Prates <iara.miranda@ufabc.edu.br>
2017-10-15 22:51:53 -0200

    ex do string to int

--- ATIVIDADE01/02Exercicios_Funcoes/ex8.hs
+++ ATIVIDADE01/02Exercicios_Funcoes/ex8.hs
@@ -1,5 +1,5 @@
 {-|
-Module      : coeficienteBinomial
+Module      : trianguloPascal
 Description : Faça uma função que calcule o elemento (i,j) do triângulo de pascal.
 License     : GPL-3
 Maintainer  : iara.miranda@ufabc.edu.br
@@ -11,9 +11,8 @@ fatorial :: Integer -> Integer
 fatorial 0 = 1
 fatorial n = fatorial (n - 1) * n
 
-coeficienteBinomial :: Integer -> Integer -> Integer
-coeficienteBinomial n p = (fatorial n) `div` (fatorial p * fatorial ( n - p ))
-
+trianguloPascal :: Integer -> Integer -> Integer
+trianguloPascal n p = (fatorial n) `div` (fatorial p * fatorial ( n - p ))
 main = do
-  print (coeficienteBinomial 8 5) -- 56
-  print (coeficienteBinomial 12 9) -- 220
+  print (trianguloPascal 3 3) -- 56
+  print (trianguloPascal 12 9) -- 220
