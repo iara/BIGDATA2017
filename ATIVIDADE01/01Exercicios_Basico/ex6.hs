8a3548c2390396d3b759c92ba8af8932a951c502
Iara Miranda Prates <iara.miranda@ufabc.edu.br>
2017-10-15 22:22:29 -0200

    ex do faa

--- ATIVIDADE01/01Exercicios_Basico/ex6.hs
+++ ATIVIDADE01/01Exercicios_Basico/ex6.hs
@@ -1,6 +1,6 @@
 {-|
-Module      : mult3
-Description :  Faça uma função que recebe um tipo Integer e retorna ele dividido por
+Module      : Faa
+Description : Faa uma função que receba um ângulo a e retorne uma tupla contendo o seno da metade desse ângulo utilizando a identidade
 Copyright   : (c) Iara Miranda, 2017
 License     : GPL-3
 Maintainer  : iara.miranda@ufabc.edu.br
@@ -10,10 +10,9 @@ Maintainer  : iara.miranda@ufabc.edu.br
 
 module Main where
 
-div2d :: Integer -> Double
-div2d x = x `div` 2
+faa :: Double -> (Double, Double)
+faa n = (sqrt ((1 - cos 2) / 2), -sqrt ((1 - cos 2) / 2))
 
 main :: IO ()
 main = do
-  print (div2d 2)
-  print (div2d 5)
+  print(faa 9.3)
