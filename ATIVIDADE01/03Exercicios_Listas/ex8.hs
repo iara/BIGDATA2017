baa863ffcfdbbed36aac329c7dbbc36e4f1dbc4f
Iara Miranda <iaramiranda@live.com>
2017-10-16 12:03:54 -0200

    Update ex.8 list 3

--- ATIVIDADE01/03Exercicios_Listas/ex8.hs
+++ ATIVIDADE01/03Exercicios_Listas/ex8.hs
@@ -1,6 +1,6 @@
 {-|
-Module      : maiorCollatzLen
-Description : Implemente uma função collatzLen x que retorna o tamanho da lista formada pela aplicação repetida de collatz sobre o valor x até que essa chegue no número 1.
+Module      : projectEuler14
+Description : Encontre o número x entre 1 e 1.000.000 que tem a maior sequência de Collatz. (Project Euler 14)
 License     : GPL-3
 Maintainer  : iara.miranda@ufabc.edu.br
 
@@ -17,8 +17,20 @@ collatz x
   | otherwise = (3 * x) + 1
 
 
+collatzLen :: Integer -> Integer -> Integer
 collatzLen value 1 = value
 collatzLen value n = collatzLen (value + 1) (collatz n)
 
+collatzLen' n = (collatzLen 1 n, n)
+
+maiorCollatzLen :: Integer -> (Integer,Integer)
+maiorCollatzLen n = maximum (map (\y ->  collatzLen' y) [1..n])
+
+projectEuler14:: Integer -> String
+projectEuler14 n = strResult
+  where
+    result = maiorCollatzLen n
+    strResult = "O numero de maior valor de sequencia eh o " ++ show (snd result) ++ ", com a sequencia de tamanho " ++ show (fst result) ++".Para o intervalo de 1 a "++ show n ++ "."
+
 main = do
-    print(collatzLen 1 13)
+    print(projectEuler14 1000000) -- "O numero de maior valor de sequencia eh o 837799, com a sequencia de tamanho 525.Para o intervalo de 1 a 1000000."
